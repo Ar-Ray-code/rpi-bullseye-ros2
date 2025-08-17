42846aa4d61049b93802236e5f3571056d050968
Ar-Ray-code <ray255ar@gmail.com>
2025-08-17 19:45:30 +0900

    Update build script to use DISTRO variable for repository URLs and add sequential executor option

--- build/ros2_ws/build.bash
+++ build/ros2_ws/build.bash
@@ -27,12 +27,12 @@ mkdir -p src/
 
 if [ ${BUILD_FULL_PKG} = true ]; then
     echo "Building full package"
-    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/main/repos/ds.repos
-    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/main/repos/realsense.repos
-    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/main/repos/rostackchan.repos
-    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/main/repos/urg.repos
-    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/main/repos/velodyne.repos
-    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/main/repos/webcam.repos
+    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/${DISTRO}/repos/ds.repos
+    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/${DISTRO}/repos/realsense.repos
+    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/${DISTRO}/repos/rostackchan.repos
+    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/${DISTRO}/repos/urg.repos
+    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/${DISTRO}/repos/velodyne.repos
+    wget https://raw.githubusercontent.com/Ar-Ray-code/rpi-bullseye-ros2/${DISTRO}/repos/webcam.repos
 
     for f in *.repos; do
         echo "---- importing $f ----"
@@ -57,6 +57,7 @@ export CPLUS_INCLUDE_PATH=${PCL_INCLUDE_PATH}:${EIGEN_INCLUDE_PATH}:$CPLUS_INCLU
 
 colcon build \
     --install-base $(pwd)/${DISTRO}/ \
+    --executor sequential \
     --packages-ignore autoware_trajectory autoware_ekf_localizer autoware_map_projection_loader autoware_test_utils autoware_planning_test_manager autoware_gnss_poser autoware_route_handler autoware_map_loader autoware_simple_pure_pursuit autoware_velocity_smoother autoware_mission_planner autoware_path_generator autoware_behavior_velocity_planner_common autoware_behavior_velocity_planner autoware_motion_velocity_planner_common autoware_motion_velocity_planner autoware_motion_velocity_obstacle_stop_module autoware_behavior_velocity_stop_line_module \
     --merge-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-march=armv8-a+crc -mtune=cortex-a72 -O3" -DCMAKE_C_FLAGS="-march=armv8-a+crc -mtune=cortex-a72 -O3"
 
